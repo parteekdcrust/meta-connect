74b2b5b03fb22764359efdd269f34d77d268a9bb
parteekdcrust <pgarg56023@gmail.com>
2023-08-22 17:02:08 +0530

    switch network button added

--- src/components/walletCard.js
+++ src/components/walletCard.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const WalletCard = () => {
   const [defaultAccount, setDefaultAccount] = useState(null);
+  const [defaultNetwork, setDefaultNetwork] = useState(null);
 
   const connWalletHandler = async () => {
     try {
@@ -10,6 +11,16 @@ const WalletCard = () => {
       });
       console.log(accounts);
       const account = accounts[0];
+      const response = await fetch("https://chainid.network/chains.json"); //fetched details of all networks
+      const data = await response.json();
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      // console.log(chainId);
+      const networkDetails = data.find((network) => {
+        // eslint-disable-next-line
+        return network.chainId == chainId;
+      });
+      console.log(networkDetails);
+      setDefaultNetwork(networkDetails.name);
       setDefaultAccount(account);
     } catch (error) {
       console.log(error);
@@ -25,12 +36,62 @@ const WalletCard = () => {
   };
   window.ethereum.on("accountsChanged", accChangedHandler);
 
+  const handleNetworkSwitch = async (networkName) => {
+    if (window.ethereum) {
+      try {
+        const response = await fetch("https://chainid.network/chains.json"); //fetched details of all networks
+        const data = await response.json();
+        const networkDetails = data.find((network) => {
+          return network.name === networkName;
+        });
+        console.log(networkDetails);
+        await window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: `0x${Number(networkDetails.chainId).toString(16)}`, //changed chainId to hexadecimal
+              chainName: networkDetails.name,
+              rpcUrls: [...networkDetails.rpc],
+              nativeCurrency: networkDetails.nativeCurrency,
+            },
+          ],
+        });
+        setDefaultNetwork(networkDetails.name);
+        // Network switched successfully
+      } catch (error) {
+        // Error while switching network
+        console.log(error);
+      }
+    } else {
+      // MetaMask is not connected
+      console.log("Metamask is not connected");
+    }
+  };
+
   return (
     <div className="walletCard">
       <h1>Connect to Metamask with window ethereum </h1>
       <button onClick={connWalletHandler}>Connect Wallet</button>
       <div>
-        <h2> Address:{defaultAccount}</h2>
+        <h2>Current Address: {defaultAccount}</h2>
+      </div>
+      <div>
+        <h2>Current Network: {defaultNetwork} </h2>
+      </div>
+      <div>
+        <button onClick={() => handleNetworkSwitch("BNB Smart Chain Mainnet")}>
+          Switch to BSC
+        </button>
+      </div>
+      <div>
+        <button onClick={() => handleNetworkSwitch("Polygon Mainnet")}>
+          Switch to Polygon Mainnet
+        </button>
+      </div>
+      <div>
+        <button onClick={() => handleNetworkSwitch("Arbitrum One")}>
+          Switch to Arbitrum One
+        </button>
       </div>
     </div>
   );
